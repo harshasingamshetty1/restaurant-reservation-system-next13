769e5840064eff1de0f65d306a28128ce1df6df1
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-11-12 23:38:54 +0530

    feat: restaurant page functionality

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import NavBar from "../../components/NavBar";
-import Header from "./components/Header";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
 import Rating from "./components/Rating";
@@ -8,15 +6,46 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import prisma from "../../db";
 
-const RestaurantDetailsPage = () => {
+interface Restaurant {
+  id: number;
+  name: string;
+  slug: string;
+  description: string;
+  images: string[];
+}
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+    select: {
+      id: true,
+      slug: true,
+      description: true,
+      images: true,
+      name: true,
+    },
+  });
+
+  if (!restaurant) throw new Error();
+  return restaurant;
+};
+const RestaurantDetailsPage = async (props: any) => {
+  console.log("🚀 ~ file: page.tsx:34 ~ RestaurantDetailsPage ~ props:", props);
+  //by default, eveery page has props sent by default which contains both the params and searchParams
+  const restaurant = await fetchRestaurantBySlug(props.params.slug);
+  console.log(
+    "🚀 ~ file: page.tsx:17 ~ RestaurantDetailsPage ~ restaurant:",
+    restaurant
+  );
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name} />
         <Rating />
-        <Description />
+        <Description description={restaurant.description} />
         <Images />
         <Reviews />
       </div>
