b5386d86525e9975dc862e06cdb624f05987af1e
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2024-05-19 12:30:50 +0530

    fixes

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { times } from "../../../../data";
+import prisma from "../../../../app/db";
+import { tr } from "date-fns/locale";
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -21,9 +24,37 @@ export default async function handler(
 
   if (!searchTimes)
     return res.status(400).json({ error: "Invalid time provided" });
-  return res.json({
-    slug,
-    day,
+
+  const { id: desiredRestaurantId } = await prisma.restaurant.findFirstOrThrow({
+    where: { slug },
+    select: {
+      id: true,
+    },
   });
+  const currentBookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+      },
+      restaurant_id: desiredRestaurantId,
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  const currentBookingsObj: { [key: string]: { [key: string]: true } } = {};
+
+  currentBookings.forEach((booking) => {
+    currentBookingsObj[booking.booking_time.toISOString()] =
+      booking.tables.reduce((previousValue, currentValue) => {
+        return { ...previousValue, [currentValue.table_id]: true };
+      }, {});
+  });
+
+  return res.json(currentBookingsObj);
 }
-//http://localhost:3000/api/restaurant/pukka-niagara/availability?time=00:30:00.000Z&day=123&partySize=8
+//http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisi-ottawa/availability?time=16:30:00.000Z&day=2023-12-24&partySize=3
